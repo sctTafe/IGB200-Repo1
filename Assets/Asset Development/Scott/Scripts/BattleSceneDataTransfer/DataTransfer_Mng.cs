9bacf8c1c6b1563173ff5f689ecd9cfbd0dc2a06
Scott B <94416375+sctTafe@users.noreply.github.com>
2023-09-08 05:55:51 +1000

    Update DataTransfer_Mng.cs
    unteseted, but should work in theory

--- Assets/Asset Development/Scott/Scripts/BattleSceneDataTransfer/DataTransfer_Mng.cs
+++ Assets/Asset Development/Scott/Scripts/BattleSceneDataTransfer/DataTransfer_Mng.cs
@@ -1,16 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using UnityEngine;
+
+/// <summary>
+/// 
+/// DOSE: 
+///     Class for transfering team Member data to battle scene.
+///     When 'fn_LoadMissionTeam' is called, TeanMember_SelectionGroupHolder_Mng is contacted, 
+///     '_selectedMissionTeam' & its members are retrived, then a holder class is instanciated and added to the  
+/// DEPENDENCY: 
+///     Calls on 'TeanMember_SelectionGroupHolder_Mng' to get Mission Team
+///     Instantiates holder classes, then addes it to the 'team' list in 'StaticData' Class
+/// NOTE:
+/// 
+/// </summary>
 
 public class DataTransfer_Mng : MonoBehaviour
 {
-    public StaticData _StaticData;
+    public bool _isDebuggingOn = false;
     public TeamMemberTransfer_Data _Prefab;
-    public TeanMember_SelectionGroupHolder_Mng _TeamMemberSelectionGroupHolder_Mng;
+    private TeanMember_SelectionGroupHolder_Mng _TeamMemberSelectionGroupHolder_Mng;
 
     public void fn_LoadMissionTeam()
     {
+        if (_isDebuggingOn) { Debug.Log("DataTransfer_Mng: fn_LoadMissionTeam Called, "); }
+
         connectToSelectionGroupHolder();
         if (_TeamMemberSelectionGroupHolder_Mng != null)
         {
@@ -18,8 +30,9 @@ public class DataTransfer_Mng : MonoBehaviour
             {
                 // Instantiate New Team Member GameObject
                  GameObject teamMember = InstantiateTeamMemberTransfereHolder(item._uID, item._classType, item._maxEnergy, item._currentEnergy);
+                
                 // Add to StaticData List
-                //_StaticData.team.Add(teamMember);
+                StaticData.team.Add(teamMember);
             }
         }
     }

0be6774f6dbe859f613b1a0f5edadecfc61d6e48
sctTafe <ba.scott.barley@gmail.com>
2023-10-11 13:38:29 +1000

    Progression Scene - UI Polishing

---
+++ Assets/Asset Development/Scott/Scripts/UI Special Elements/OnPointerRectTransfromResize_UI.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class OnPointerRectTransfromResize_UI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private RectTransform _buttonTrans;
+    [SerializeField] private float _scaleMultiplier_Pct = 5f; //5%
+
+    public void Start()
+    {
+        //DOSE: returns the rectTransform the script is attached to, if 'nuil' (not player set)
+        //_buttonTrans ??= GetComponent<RectTransform>();
+        if (_buttonTrans == null)
+            _buttonTrans = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _buttonTrans.DOScale((1f+0.01f*_scaleMultiplier_Pct), 0.2f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _buttonTrans.DOScale(1f, 0.2f);
+    }
+}

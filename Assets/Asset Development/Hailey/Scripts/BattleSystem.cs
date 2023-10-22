bf1f92c8bb51973cdf5d23c0038ae880116c29b5
sctTafe <ba.scott.barley@gmail.com>
2023-10-22 11:50:06 +1000

    Battle Celebration - Done

--- Assets/Asset Development/Hailey/Scripts/BattleSystem.cs
+++ Assets/Asset Development/Hailey/Scripts/BattleSystem.cs
@@ -320,9 +320,9 @@ public class BattleSystem : MonoBehaviour
             StaticData.team.Clear(); //reset the team list
         }
         
-        yield return new WaitForSeconds(2f);
-
         _OnBattleEnd?.Invoke(); // Invoke Event                             // SCOTT EDIT - So that functions can be called for integrating to two scene
+
+        yield return new WaitForSeconds(5f);                                // SCOTT EDIT - changed to 5 seconds to give a change for the player to read the Success / Lose Info   
         SceneManager.LoadScene(scene);
     }
 
@@ -444,4 +444,7 @@ public class BattleSystem : MonoBehaviour
     {
         debugMode = false;
     }
+
+    public void fn_LoadNextScene() => SceneManager.LoadScene(scene);
+
 }
